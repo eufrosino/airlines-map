e854f2ad1287924621264153de3f2bfab79615f2
Eufrosino <eufrosinoveloso@gmail.com>
2021-04-30 13:48:04 -0500

    filter by airline

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Table from './components/Table';
 import DATA from './data';
 
+const AirlineSelect = ({
+  options = [],
+  valueKey = '',
+  titleKey = '',
+  value = 'all',
+  allTitle = 'all',
+  onSelect = (_) => null,
+}) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+    onSelect(event.target.value);
+  };
+
+  let optionElements = [
+    <option key='all' value='all'>
+      {allTitle}
+    </option>
+  ]
+
+  options.forEach(option => {
+    const value = option[valueKey]
+    optionElements.push(
+      <option key={value} value={value}>
+        {option[titleKey]}
+      </option>
+    );
+  });
+
+  return (
+    <select value={value} onChange={handleChange}>
+      {optionElements}
+    </select>
+  )
+}
+
 const App = () => {
+  const [airline, setAirline] = useState('all');
 
   const formatValue = (property, value) => {
-    if (property === "airline") {
+    if (property === 'airline') {
       return DATA.getAirlineById(value).name;
     }
-
     return DATA.getAirportByCode(value).name;
+  };
+
+  const filteredRoutes = DATA.routes.filter((route) => {
+    return (route.airline === airline || airline === 'all');
+  });
+
+  const filteredAirlines = DATA.airlines.map((airline) => {
+    const active = !!filteredRoutes.find(
+      (route) => route.airline === airline.id
+    );
+    return Object.assign({}, airline, { active });
+  });
+
+  const airlineSelected = (value) => {
+    if (value !== 'all') {
+      value = Number(value);
+    }
+    setAirline(value);
   };
 
   const columns = [
@@ -24,11 +77,22 @@ const App = () => {
       <header className='header'>
         <h1 className='title'>Airline Routes</h1>
       </header>
+      <p>
+        Show routes on
+        <AirlineSelect
+          options={filteredAirlines}
+          valueKey='id'
+          titleKey='name'
+          allTitle='All Airlines'
+          value={airline}
+          onSelect={airlineSelected}
+        />
+      </p>
       <section>
         <Table
-          className="routes-table"
+          className='routes-table'
           columns={columns}
-          rows={DATA.routes}
+          rows={filteredRoutes}
           format={formatValue}
         />
       </section>
